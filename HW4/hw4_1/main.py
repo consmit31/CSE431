fcd051a33d44f7eb3ffb4a646b8076ced8641df0
Conner Smith <basketballbob2003@gmail.com>
2024-03-11 15:40:07 -0400

    HW4 Q1 revised

--- HW4/hw4_1/main.py
+++ HW4/hw4_1/main.py
@@ -8,20 +8,12 @@ C.sort(reverse = True)
 total_damage = 0
 multiplier = 1
 
-start_index = 0
-if m == n:
-    total_damage += sum(C)
-while m > n:
-    fights = C[start_index:start_index+n]
-    total_damage += sum(fights) * multiplier
+for i in range(m):
+    total_damage += multiplier * C[i]
+    if (i+1)%n == 0:
+        multiplier += 1
 
-    m -= n
-    start_index += n
-    multiplier += 1
 
-fights = C[start_index:]
-if fights and m != n:
-    total_damage += sum(fights) * multiplier
 
 print(total_damage)
 
