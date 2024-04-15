7c9be66d45b81099abd8f16f55739fff3fb1d1c1
Conner Smith <basketballbob2003@gmail.com>
2024-04-15 17:11:02 -0400

    HW6 Q1

--- HW6/hw6_1/main.py
+++ HW6/hw6_1/main.py
@@ -1,5 +1,3 @@
-import random
-
 class Person:
 
     def __init__(self, id : int, skills : set, skill_count : int) -> None:
@@ -8,66 +6,42 @@ class Person:
         self.skill_count = skill_count
 
     def __str__(self) -> str:
-        return(f"ID: {self.id}\n Skills: {self.skills}\n--------")   
+        return(f"ID: {self.id}\n Skills: {self.skills}\n--------\n")   
 
     def __repr__(self) -> str:
         return self.__str__()
-    
-class People:
-    def __init__(self) -> None:
-        self.people
 
-def greedy_set_cover(universe, sets):
+def greedy_set_cover(skills, people):
     covered = set()
     cover = []
 
-    while covered != universe:
-        max_set = max(sets, key=lambda s: len(s - covered))
-        cover.append(max_set)
-        covered |= max_set
+    while covered != skills:
+        max_person = max(people, key=lambda p: len(p.skills - covered)) 
+        cover.append(max_person)
+        covered |= (max_person.skills)
 
     return cover
 
-def local_search_set_cover(universe, sets):
-    solution = greedy_set_cover(universe, sets)
-    uncovered = universe - set().union(*solution)
+def main():
+    N, K = map(int, input().split())
+    people = []
+    total_skills = set(range(K))
+    for i in range(N):
+        skill_count = int(input())
+        skills = set(map(int, input().split()))    
+        person = Person(i, skills, skill_count)
 
-    while uncovered:
-        best_improvement = 0
-        best_set = None
+        people.append(person)
 
-        for s in sets:
-            if s not in solution:
-                improvement = len(uncovered & s)
-                if improvement > best_improvement:
-                    best_improvement = improvement
-                    best_set = s
+    selected = greedy_set_cover(total_skills, people)
+    print(len(selected))
 
-        if best_set:
-            solution.append(best_set)
-            uncovered -= best_set
-        else:
-            break
-
-    return solution
-
-# Example usage:
-universe = {1, 2, 3, 4, 5}
-sets = [{1, 2, 3}, {2, 3, 4}, {4, 5}]
-
-N, K = map(int, input().split())
-people = []
-total_skills = set(range(K))
-for i in range(N):
-    skill_count = int(input())
-    skills = set(map(int, input().split()))    
-    person = Person(i, skills, skill_count)
-
-    people.append(person)
-
-print(people, total_skills)
+    ids = []
+    for p in selected:
+        ids.append(str(p.id))
+    print(" ".join(ids))
 
 
+main()
 
-print("Greedy set cover solution:", greedy_set_cover(universe, sets))
-print("Local search set cover solution:", local_search_set_cover(universe, sets))
+
